9b99ea6c37ab6f3b19bc000c00f1216b87c4b595
James Miller <james.rex.miller.4@gmail.com>
2020-03-18 20:01:53 -0600

    Add anchor tags for About, Projects, Resume + animations

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,6 +4,9 @@ import './Header.scss';
 const Header = () => {
   return ( 
     <header>
+      <a href="/about">About</a>
+      <a href='/projects'>Projects</a>
+      <a href='/resume'>Resume</a>
     </header>
   );
 }
