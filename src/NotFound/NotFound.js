faaba30964bd30ce464125876192ed6206c2dbd5
James Miller <james.rex.miller.4@gmail.com>
2020-03-18 19:47:30 -0600

    Add NotFound component

---
+++ src/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.scss';
+
+const NotFound = () => {
+  return ( 
+    <section className="not-found">
+      <h2>404</h2>
+    </section>
+  );
+}
+
+export default NotFound;
