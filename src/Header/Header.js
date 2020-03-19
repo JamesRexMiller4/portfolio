f4267371d1f89904278b29e2ca7293753cf8dfba
James Miller <james.rex.miller.4@gmail.com>
2020-03-18 21:06:41 -0600

    Update header with nav bar and logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Header.scss';
+import logo from '../images/tyrannosaurus-rex.svg';
 
 const Header = () => {
   return ( 
     <header>
-      <a href="/about">About</a>
-      <a href='/projects'>Projects</a>
-      <a href='/resume'>Resume</a>
+      <img src={logo} alt='t-rex skull'/>
+      <nav>
+        <a href="/about">About</a>
+        <a href='/projects'>Projects</a>
+        <a href='/resume'>Resume</a>
+      </nav>
     </header>
   );
 }
- 
+
 export default Header;
