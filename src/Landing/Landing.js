ac2467891c3941e02a7c2e847b2c3e848a782c59
James Miller <james.rex.miller.4@gmail.com>
2020-03-19 18:02:35 -0600

    Move Landing into Home component

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,31 +1,17 @@
 import React from 'react';
+import BabyYoda from '../BabyYoda/BabyYoda';
 import './Landing.scss';
-import Header from '../Header/Header';
 
 const Landing = () => {
-  return ( 
+  return (
     <main>
-      <Header />
       <section className='landing-section'>
         <div className='div-message'>
           <h2>WELCOME</h2>
           <h3>TO JAMES REX MILLER IV's PERSONAL SITE</h3>
         </div>
         <div className='div-colored'>
-          <div className='baby-yoda'>
-            <div id="y-body">
-            <div id="y-head">
-            <div id="left-eye"></div>
-            <div id="right-eye"></div>
-            <div id="mouth"></div>
-            <div id="left-ear"></div>
-            <div id="right-ear"></div>
-            </div>
-            <div id="cup">
-              <div id="steam">~</div>
-            </div>
-            </div>
-          </div>
+          <BabyYoda/>
         </div>
       </section>
     </main>
