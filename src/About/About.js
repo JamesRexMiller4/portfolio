518617888262852311697d901455f9add29e7189
James Miller <james.rex.miller.4@gmail.com>
2020-03-25 21:58:21 -0600

    Add About component

---
+++ src/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './About.scss';
+
+const About = () => {
+  return ( 
+    <main>
+      <Header />
+    </main>
+  );
+}
+ 
+export default About;
