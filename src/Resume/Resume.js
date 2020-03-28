55a9779b8605cf327143ffc3ff1e282d65a25a1e
James Miller <james.rex.miller.4@gmail.com>
2020-03-28 09:26:29 -0600

    Add Resume Route, add image element, src, and styles

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Resume.scss';
+import resume from '../images/resume-photo.png';
 
 const Resume = () => {
   return ( 
-    <main>
-
+    <main className='resume-main'>
+      <img src={resume} alt='resume'/>
     </main>
   );
 }
