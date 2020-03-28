cef34da3f7481a33ba4cad33c8b1743e04db7014
James Miller <james.rex.miller.4@gmail.com>
2020-03-28 10:14:38 -0600

    Modify scaling styles and add download button with styles

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Resume.scss';
 import resume from '../images/resume-photo.png';
+import resumePDF from '../images/JAMES-R-MILLER-IV-Resume.pdf';
 
 const Resume = () => {
   return ( 
     <main className='resume-main'>
       <img src={resume} alt='resume'/>
+      <a className='download-link' href={resumePDF} download>DOWNLOAD PDF</a>
     </main>
   );
 }
