af554921b770b8a140e3932bb5e67dd0c49c8e6c
James Miller <james.rex.miller.4@gmail.com>
2020-03-31 13:53:36 -0600

    Add tyles and functionality for mask title effects

--- src/ProjectCard/ProjectCard.js
+++ src/ProjectCard/ProjectCard.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './ProjectCard.scss';
+import condensateDash from '../images/condensate-dash.png';
+// import condensateGif from '../images/condensate-gif.gif';
 
 const ProjectCard = (props) => {
+  const titleDivRef = useRef(null);
+
+  const mouseMove = () => {
+    const width = titleDivRef.current.clientWidth;
+    const height = titleDivRef.current.clientHeight;
+    console.log(width, height);
+  }
   return ( 
     <section className='project-card-section'>
       <div className='left-half-project-card-div'>
-        <div className='title-div'>
+        <div className='titleDiv' onMouseMove={mouseMove} ref={titleDivRef}>
           <div className='title-wrapper-div'>
-            <h2>{props.title}</h2>
-            <div>Divider</div>
+            <h2>{props.title1}</h2>
+            <div className='title2-div'>
+              <div className='div-divider'></div>
+              <h2>{props.title2}</h2>
+            </div>
           </div>
           <div className='clone title-wrapper-div'>
-            <h2>{props.title}</h2>
-            <div>Divider</div>
+            <h2>{props.title1}</h2>
+            <div className='clone-title2-div'>
+              <div className='clone-divider div-divider'></div>
+              <h2>{props.title2}</h2>
+            </div>
           </div>
         </div>
         <div className='summary-div'>
@@ -20,7 +35,8 @@ const ProjectCard = (props) => {
         </div>
       </div>
       <div className='right-half-project-card-div'>
-        <iframe title={props.title} src={props.url}></iframe>
+        {/* <img src={condensateDash} alt='condensate-dashboard'/> */}
+        <div className='image-gif-div'></div>
       </div>
 
     </section>
