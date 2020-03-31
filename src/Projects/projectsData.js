73b9b3eb19c25a61937c272f5e17200ca33f45d9
James Miller <james.rex.miller.4@gmail.com>
2020-03-31 13:53:15 -0600

    Create projectsData

--- src/Projects/projectsData.js
+++ src/Projects/projectsData.js
@@ -1,7 +1,10 @@
-export const projectsData = [
+const projectsData = [
   {
-    title: 'CONDENSATE',
+    title1: 'CON',
+    title2: 'DENSATE',
     url: 'https://condensate-app.herokuapp.com/',
     description: 'A new way to brainstorm'
   }
 ]
+
+module.exports = projectsData;
