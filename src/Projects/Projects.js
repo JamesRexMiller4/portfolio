2db7208cd43a5f97b6663afb368c66ce6b776f41
James Miller <james.rex.miller.4@gmail.com>
2020-03-31 11:01:47 -0600

    Add ProjectCard to Projects component

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Projects.scss';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 const Projects = () => {
+  // let projects = projectData.map()
   return ( 
     <main>
-
+      
     </main>
   );
 }
