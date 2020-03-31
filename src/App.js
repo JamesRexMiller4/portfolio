02e752a9dbd4d4b3014ac617ebe8efebb12987c0
James Miller <james.rex.miller.4@gmail.com>
2020-03-31 13:52:51 -0600

    Add Route for Projects

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './About/About';
 import Header from './Header/Header';
 import Home from './Home/Home';
 import NotFound from './NotFound/NotFound';
+import Projects from './Projects/Projects';
 import Resume from './Resume/Resume';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <Switch>
       <Route exact path={'/'} render={() => <Home />} />
       <Route path={'/about'} render={() => <About />} />
+      <Route path={'/projects'} render={() => <Projects />} />
       <Route path={'/resume'} render={() => <Resume />} />
       <Route component={() => <NotFound />} />
     </Switch>
